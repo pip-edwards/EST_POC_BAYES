290e686e702301c14f945074bffa533072d395ab
pip-edwards <pe1n24@soton.ac.uk>
2026-03-09 15:27:22 +0000

    Create make_climatologies.py

---
+++ make_climatologies.py
@@ -0,0 +1,17 @@
+#%%
+#make the climatologies for making maps and global estimates
+
+#packages
+
+
+#%%
+#SST
+
+#%%
+#Chla
+
+
+#%%
+#Depth
+
+#this is obviously not a climatology but it is still needed!
